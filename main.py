cb155011bcdfdb41d8f2d77564d10e387e8d41a3
Nala <najlabineid@gmail.com>
2024-07-17 13:55:54 +0300

    Addded create purchase and generate invoice as a PDF APIs

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import FastAPI, HTTPException, Depends, status, Response
 from pydantic import BaseModel
 from typing import Annotated
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import joinedload
+from fpdf import FPDF
+import os
+from fastapi.responses import FileResponse
+
+
 
 
 app = FastAPI()
@@ -15,6 +20,7 @@ models.Base.metadata.create_all(bind=engine)
 class UserBase(BaseModel):
     username: str
     email: str
+ 
 
 class PostBase(BaseModel):
     title: str
@@ -22,6 +28,20 @@ class PostBase(BaseModel):
     user_id: int
 
 
+class PurchaseBase(BaseModel):
+    item: str
+    quantity: int
+    price: float
+    user_id: int
+
+
+class InvoiceBase(BaseModel):
+    purchase_id: int
+    sub_total: float
+    total: float
+    tax: float
+    customer_name: str 
+    customer_email: str 
 
 
 def get_db():
@@ -155,19 +175,58 @@ async def get_user_with_posts(user_id: int, db: db_dependency):
     return user_with_posts
 
 
-
-
+@app.post("/purchases/")
+async def create_purchase(purchase: PurchaseBase, db: db_dependency):
+    db_purchase = models.Purchase(**purchase.dict())
+    db.add(db_purchase)
+    db.commit()
+    db.refresh(db_purchase)
     
+    save_invoice(db_purchase, db)
+   
+
+def save_invoice(db_purchase: PurchaseBase, db):
+    user = db.query(models.User).get(db_purchase.user_id)
+
+    sub_total = db_purchase.price * db_purchase.quantity
+    tax = sub_total * 0.15
+    total = tax + sub_total
+
+    db_invoice = models.Invoice(
+        customer_email=user.email,
+        customer_name=user.username,
+        purchase_id=db_purchase.id,
+        sub_total=sub_total,
+        tax = tax,
+        total=total
+    )
+
+    db.add(db_invoice)
+    db.commit()
+    db.refresh(db_invoice)
 
 
+# Generate the invoice as a PDF
+@app.get("/invoice/{invoice_id}")
+async def get_invoice_as_pdf(invoice_id: int, db: db_dependency):
+    invoice = db.query(models.Invoice).filter(models.Invoice.id == invoice_id).first()
+    if invoice is None:
+        raise HTTPException(status_code=404, detail='Invoice not found')
 
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
 
+    pdf.cell(0, 20, "Invoice Details", align="C", ln=True) 
+    pdf.cell(0, 10, f"Invoice ID: {invoice.id}", ln=True)
 
+    pdf.cell(0, 10, f"Customer Name:  {invoice.customer_name}", ln=True)
+    pdf.cell(0, 10, f"Customer Email:  {invoice.customer_email}", ln=True)
+    pdf.cell(0, 10, f"Sub Total amount:  ${invoice.sub_total:.2f}", ln=True)
+    pdf.cell(0, 10, f"Tax amount (15%):  ${invoice.tax:.2f}", ln=True)
+    pdf.cell(0, 10, f"Total amount:  ${invoice.total:.2f}", ln=True)
 
-
-
-
-
-
-
+    pdf_file = f"invoice_{invoice_id}.pdf"
+    pdf.output(pdf_file)
+    return FileResponse(pdf_file, media_type="application/pdf", filename=pdf_file)
 
--- models.py
+++ models.py
@@ -1,16 +1,16 @@
-from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, String,Float, ForeignKey,DateTime
 from database import Base
 from sqlalchemy.orm import relationship
+
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True)
-    email = Column(String(50),unique=True)
+    email = Column(String(50), unique=True)
     posts = relationship("Post", back_populates="user")
-
-
+    purchases = relationship("Purchase", back_populates="user")
 
 
 class Post(Base):
@@ -20,8 +20,29 @@ class Post(Base):
     title = Column(String(100))
     content = Column(String(500))
     user_id = Column(Integer, ForeignKey('users.id'))
-
     user = relationship("User", back_populates="posts")
 
-    
 
+class Purchase(Base):
+    __tablename__ = 'purchases'
+
+    id = Column(Integer, primary_key=True, index=True)
+    item = Column(String(100))
+    quantity = Column(Integer)
+    price = Column(Float)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="purchases")
+    #invoice = relationship("Invoice", uselist=False, back_populates="purchase")
+
+
+class Invoice(Base):
+    __tablename__ = 'invoices'
+
+    id = Column(Integer, primary_key=True, index=True)
+    purchase_id = Column(Integer, unique=True)
+    #purchase = relationship("Purchase", back_populates="invoice")
+    sub_total =Column(Float)
+    total = Column(Float)
+    tax = Column(Float)
+    customer_name = Column(String(50))
+    customer_email = Column(String(50))
